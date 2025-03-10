45383c4fb8d99bfd7ce39f724d94e0e215f6ea9f
Japods <jcodepod@gmail.com>
2025-03-10 01:32:39 -0400

    chore: initialize project and configure remote microfrontend (Harry Potter)

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Remoto Harry Potter</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), federation({
+    name: 'n5-remote-hp',
+    filename: 'remoteEntry.js',
+    exposes: {
+      './Header': './src/Header.jsx'
+    },
+    remotes: {},
+    shared: ['react', 'react-dom'],
+  })],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+})
